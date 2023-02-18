515bd323c6676c44e2d046f4fd0782c7760debd7
Ifetex147 <ifetexekeson147@gmail.com>
2023-02-18 05:39:20 -0800

    my low commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - A program that prints puts function in a line
+ * reurn:0 when the program returns the val zero that shows we succeeded
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
