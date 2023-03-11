eafb3c21f03873db695abd5b0ba337ded0e4dd7b
Ifetex147 <ifetexekeson147@gmail.com>
2023-03-10 16:13:15 -0800

    my task

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -11,7 +11,6 @@ char *_strcat(char *dest, char *src)
 	int i;
 	int j;
 
-
 	i = 0;
 	while (dest[i] != '\0')
 	{
@@ -25,7 +24,7 @@ char *_strcat(char *dest, char *src)
 		j++;
 	}		
 
-
 	dest[i] = '\0';
 	return (dest);
 }
+
